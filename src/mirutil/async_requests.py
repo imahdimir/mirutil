ae38f9c2f48085bac87f16acfcf07ff898fc061f
imahdimir <imahdimir@gmail.com>
2022-09-03 18:54:47 +0430

    added headers param to async text req

--- src/mirutil/async_requests.py
+++ src/mirutil/async_requests.py
@@ -1,5 +1,6 @@
-##
+"""
 
+    """
 
 import asyncio
 from functools import partial
@@ -10,59 +11,60 @@ from aiohttp import ClientSession
 
 nest_asyncio.apply()
 
-##
 # getting resp text async funcs
-
-async def get_a_resp_text_async(url , trust_env , params , verify_ssl) :
-  async with ClientSession(trust_env = trust_env) as ses :
-    async with ses.get(url ,
-                       params = params ,
-                       verify_ssl = verify_ssl) as resp :
-      if resp.status == 200 :
-        return await resp.text()
+async def get_a_resp_text_async(url ,
+                                trust_env ,
+                                params ,
+                                verify_ssl ,
+                                headers) :
+    async with ClientSession(trust_env = trust_env) as ses :
+        async with ses.get(url ,
+                           params = params ,
+                           headers = headers ,
+                           verify_ssl = verify_ssl , ) as resp :
+            if resp.status == 200 :
+                return await resp.text()
 
 async def get_reps_texts_async(urls ,
                                trust_env = False ,
                                params = None ,
-                               verify_ssl = True
-                               ) :
-  fu = partial(get_a_resp_text_async ,
-               trust_env = trust_env ,
-               params = params ,
-               verify_ssl = verify_ssl)
+                               headers = None ,
+                               verify_ssl = True) :
+    fu = partial(get_a_resp_text_async ,
+                 trust_env = trust_env ,
+                 params = params ,
+                 headers = headers ,
+                 verify_ssl = verify_ssl)
 
-  co_tasks = [fu(x) for x in urls]
+    co_tasks = [fu(x) for x in urls]
 
-  return await asyncio.gather(*co_tasks)
+    return await asyncio.gather(*co_tasks)
 
-##
 # getting resp json async funcs
 
 async def get_a_resp_json_async(url ,
                                 trust_env ,
                                 params ,
                                 verify_ssl ,
-                                content_type
-                                ) :
-  async with ClientSession(trust_env = trust_env) as ses :
-    async with ses.get(url ,
-                       params = params ,
-                       verify_ssl = verify_ssl) as resp :
-      if resp.status == 200 :
-        return await resp.json(content_type = content_type)
+                                content_type) :
+    async with ClientSession(trust_env = trust_env) as ses :
+        async with ses.get(url ,
+                           params = params ,
+                           verify_ssl = verify_ssl) as resp :
+            if resp.status == 200 :
+                return await resp.json(content_type = content_type)
 
 async def get_reps_jsons_async(urls ,
                                trust_env = False ,
                                params = None ,
                                verify_ssl = True ,
-                               content_type = None
-                               ) :
-  fu = partial(get_a_resp_json_async ,
-               trust_env = trust_env ,
-               params = params ,
-               verify_ssl = verify_ssl ,
-               content_type = content_type)
+                               content_type = None) :
+    fu = partial(get_a_resp_json_async ,
+                 trust_env = trust_env ,
+                 params = params ,
+                 verify_ssl = verify_ssl ,
+                 content_type = content_type)
 
-  co_tasks = [fu(x) for x in urls]
+    co_tasks = [fu(x) for x in urls]
 
-  return await asyncio.gather(*co_tasks)
+    return await asyncio.gather(*co_tasks)
