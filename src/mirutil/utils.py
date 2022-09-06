8180aa3e5aafb46a537c61c7a7d8569d487546f6
imahdimir <imahdimir@gmail.com>
2022-09-07 02:44:06 +0430

    v4.0 - delete get_tok_if_accessible function + some overhaul and adding docstrings + printing only first 3 plus last 2 tuples in the ret_clusters function.

--- src/mirutil/utils.py
+++ src/mirutil/utils.py
@@ -2,15 +2,15 @@
 
   """
 
-from pathlib import Path
+def ret_clusters_indices(iterable , cluster_size = 100) :
+    """ clusters the iterable into clusters of size cluster_size
+        and returns the start and end indices of each cluster
+        in a list of tuples
 
-
-def get_tok_if_accessible(fp) :
-    if Path(fp).exists() :
-        with open(fp , 'r') as fi :
-            return fi.read().strip()
-
-def return_clusters_indices(iterable , cluster_size = 100) :
+    :param iterable: the iterable to be clustered
+    :param cluster_size: the size of each cluster
+    :return: list of tuples of start and end indices of each cluster
+    """
     intdiv = len(iterable) // cluster_size
 
     cis = [x * cluster_size for x in range(0 , intdiv + 1)]
@@ -32,13 +32,16 @@ def return_clusters_indices(iterable , cluster_size = 100) :
         se = (si , ei)
         se_tuples.append(se)
 
-    print(se_tuples)
+    prt = se_tuples[0 :3] + se_tuples[-2 :]
+    for el in prt :
+        print(el)
     return se_tuples
 
-def list_all_files_recursively(root_dir) :
-    """
-    :param root_dir:
-    :return:
+def list_all_files_recursively_in_all_subdirs(root_dir) :
+    """ lists all files in all subdirectories of the root_dir
+
+    :param root_dir: the root to start the search from
+    :return: list of pathes to all files in all subdirectories of the root_dir
     """
     import os
 
@@ -46,9 +49,9 @@ def list_all_files_recursively(root_dir) :
     for root , dirs , files in os.walk(root_dir) :
         for file in files :
             all_files.append(os.path.join(root , file))
-
     return all_files
 
 def print_list_as_dict_fmt(lst) :
+    """ prints the list as a dictionary format with the elements as keys"""
     for item in lst :
-        print('"' + item + '":None,')
+        print('"' + item + '" : None ,')
