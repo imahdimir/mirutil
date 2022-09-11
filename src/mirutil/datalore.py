cc183f3d8ffae227587fd96420f347f8bf369500
imahdimir <imahdimir@gmail.com>
2022-09-11 23:33:08 +0430

    v4.8 - added clone_repo_in_datalore_cwd()

--- src/mirutil/datalore.py
+++ src/mirutil/datalore.py
@@ -5,6 +5,8 @@
 import shutil
 from pathlib import Path
 
+from githubdata import GithubData
+
 
 def rm_all_in_datalore_cwd() :
     """Remove all files and directories in the current working directory.
@@ -26,6 +28,24 @@ def rm_all_in_datalore_cwd() :
 
     _ = [x.unlink() for x in fps if not x.is_dir()]
     _ = [shutil.rmtree(x) for x in fps if x.is_dir()]
+
+    fps = Path.cwd().glob('*')
+    print(list(fps))
+
+def clone_repo_in_datalore_cwd(repo_url) :
+    """Clone a git repository into the current working directory.
+
+    This is useful for cloning a git repository into the current working
+    directory of a datalore notebook.
+    """
+    print('cwd: ' + str(Path.cwd()))
+    print('repo_url: ' + repo_url)
+
+    rp = GithubData(repo_url)
+
+    rp.local_path = Path.cwd()
+
+    rp.overwriting_clone(overwrite = False)
 
     fps = Path.cwd().glob('*')
     print(list(fps))
