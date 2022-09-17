b08e7f1822f892e9ebab0eb5f3822b9ed44753b0
imahdimir <imahdimir@gmail.com>
2022-09-17 14:27:26 +0430

    v5 - changes in latex.py

--- src/mirutil/latex.py
+++ src/mirutil/latex.py
@@ -2,36 +2,30 @@
 
     """
 
-import os
 from gitignore_parser import parse_gitignore
 
 
-def ret_pathes_4_latex_dirtree_forest(root_dir ,
-                                      gitignore_path ,
-                                      only_dirs = True) :
+def make_txt4_forest_tree(root_dir , gitignore_path , only_dirs = True) :
     flt = parse_gitignore(gitignore_path)
 
     def _build(cur_dir , ignore_filter , only_dirs) :
-        res = "\n[" + os.path.basename(cur_dir)
+        res = "\n[" + cur_dir.name
 
-        lst = os.listdir(cur_dir)
-        print(lst)
+        ls = cur_dir.glob('*')
 
-        dirs = [x for x in lst if os.path.isdir(os.path.join(cur_dir , x))]
-        files = [x for x in lst if os.path.isfile(os.path.join(cur_dir , x))]
+        dirs = [x for x in ls if x.is_dir()]
+        files = [x for x in ls if x.is_dir()]
 
         dirs = sorted(dirs)
         files = sorted(files)
 
         for d in dirs :
-            if not ignore_filter(os.path.join(cur_dir , d)) :
-                res += _build(
-                        os.path.join(cur_dir , d) , ignore_filter , only_dirs
-                        )
+            if not ignore_filter(d) :
+                res += _build(d , ignore_filter , only_dirs)
                 print(res)
 
         for f in files :
-            if not only_dirs and not ignore_filter(os.path.join(cur_dir , f)) :
+            if not only_dirs and not ignore_filter(f) :
                 res += "[" + f + ", is file]"
 
         return res + "]"
