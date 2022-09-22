79a46be76b4767462a39a5379fe0d7cc03945f29
imahdimir <imahdimir@gmail.com>
2022-09-22 20:59:03 +0330

    v5.2 - simplification of make_tex_code_4_forest_tree_fr_rootdir()

--- src/mirutil/latex.py
+++ src/mirutil/latex.py
@@ -5,29 +5,15 @@
 from gitignore_parser import parse_gitignore
 
 
-def make_txt4_forest_tree(root_dir , gitignore_fp, only_dirs = True) :
+def make_tex_code_4_forest_tree_fr_rootdir(root_dir , gitignore_fp) :
     flt = parse_gitignore(gitignore_fp)
-
-    def _build(cur_dir , ignore_filter , only_dirs) :
+    def _build(cur_dir , ignore_filter) :
         res = "\n[" + cur_dir.name
-
         ls = cur_dir.glob('*')
-
         dirs = [x for x in ls if x.is_dir()]
-        files = [x for x in ls if not x.is_dir()]
-
         dirs = sorted(dirs)
-        files = sorted(files)
-
         for d in dirs :
             if not ignore_filter(d) :
-                res += _build(d , ignore_filter , only_dirs)
-                print(res)
-
-        for f in files :
-            if not only_dirs and not ignore_filter(f) :
-                res += "[" + f + "]"
-
+                res += _build(d , ignore_filter)
         return res + "]"
-
-    return _build(root_dir , flt , only_dirs).strip()
+    return _build(root_dir , flt).strip()
