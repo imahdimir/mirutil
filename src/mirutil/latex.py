7976ba5409554b8be28820621f7a5ff8a9aec762
imahdimir <imahdimir@gmail.com>
2022-09-22 20:46:28 +0330

    v5.1 - fixes in latex.py

--- src/mirutil/latex.py
+++ src/mirutil/latex.py
@@ -5,8 +5,8 @@
 from gitignore_parser import parse_gitignore
 
 
-def make_txt4_forest_tree(root_dir , gitignore_path , only_dirs = True) :
-    flt = parse_gitignore(gitignore_path)
+def make_txt4_forest_tree(root_dir , gitignore_fp, only_dirs = True) :
+    flt = parse_gitignore(gitignore_fp)
 
     def _build(cur_dir , ignore_filter , only_dirs) :
         res = "\n[" + cur_dir.name
@@ -14,7 +14,7 @@ def make_txt4_forest_tree(root_dir , gitignore_path , only_dirs = True) :
         ls = cur_dir.glob('*')
 
         dirs = [x for x in ls if x.is_dir()]
-        files = [x for x in ls if x.is_dir()]
+        files = [x for x in ls if not x.is_dir()]
 
         dirs = sorted(dirs)
         files = sorted(files)
@@ -26,7 +26,7 @@ def make_txt4_forest_tree(root_dir , gitignore_path , only_dirs = True) :
 
         for f in files :
             if not only_dirs and not ignore_filter(f) :
-                res += "[" + f + ", is file]"
+                res += "[" + f + "]"
 
         return res + "]"
 
