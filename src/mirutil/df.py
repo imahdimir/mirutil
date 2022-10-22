0a8497ebb1928681befef4c2e3f0e812714aa101
imahdimir <imahdimir@gmail.com>
2022-10-22 08:13:58 +0330

    v13.1.0, fix a bug in df.ret_north_west_of_indices()

--- src/mirutil/df.py
+++ src/mirutil/df.py
@@ -98,13 +98,13 @@ def find_all_df_locs_eq_val(df: pd.DataFrame , val) -> pd.MultiIndex :
     s = _df.stack()
     return s.index
 
-def ret_north_west_of_multiindex(mi: pd.MultiIndex) :
+def ret_north_west_of_indices(mi: pd.MultiIndex) :
     df = mi.to_frame()
     if df.empty :
         return
     df = df.sort_values(by = [0 , 1] , ascending = False)
-    r = df.iloc[0]
-    return r.index
+    r = df.iloc[[0]]
+    return r.index[0]
 
 def ret_mask_indices(msk) :
     return msk[msk].index
