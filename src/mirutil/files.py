7c8cb5058e9931d51bd041c1d6f5104a41612b7c
imahdimir <imahdimir@gmail.com>
2022-10-23 18:34:37 +0330

    v14.9.2

--- src/mirutil/files.py
+++ src/mirutil/files.py
@@ -16,8 +16,12 @@ def read_json_file(fp) :
     return json.loads(read_txt_file(fp))
 
 def write_to_file(content , fp , mode = 'w' , encoding = 'utf-8') :
-    with open(fp , mode , encoding = encoding) as f :
-        f.write(content)
+    if mode == 'w' :
+        with open(fp , mode , encoding = encoding) as f :
+            f.write(content)
+    elif mode == 'wb' :
+        with open(fp , mode) as f :
+            f.write(content)
 
 def write_txt_to_file(txt , fp) :
     write_to_file(txt , fp)
