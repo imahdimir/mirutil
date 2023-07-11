63973ecfc4c1bfb7de27ce300b53e66158c52903
imahdimir <imahdimir@gmail.com>
2023-07-11 15:03:55 +0330

    added run_modules module 22.1.0

---
+++ src/mirutil/run_modules.py
@@ -0,0 +1,34 @@
+"""
+
+    """
+
+from pathlib import Path
+import runpy
+
+def get_modules_to_run_from_path(modules_dir: Path | str) -> list[Path] :
+    """
+
+        """
+    # get all .py files in modules_dir
+    pys = Path(modules_dir).glob('*.py')
+
+    # filter those that start with '_'
+    moduls = [x for x in pys if x.name.startswith('_')]
+
+    # sort them based on the number after _
+    moduls = sorted(moduls)
+
+    return moduls
+
+def run_modules_from_dir_in_order(modules_dir: Path | str) -> None :
+    """
+
+        """
+
+    ms = get_modules_to_run_from_path(modules_dir)
+
+    # run modules in order
+    for m in ms :
+        print('Running : ' , m.name)
+
+        runpy.run_path(str(m) , run_name = '__main__')
